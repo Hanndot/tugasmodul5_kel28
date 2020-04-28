abbbc773503296ff4574f7db448197202902a162
Farhan Athaullah <athaullahfarhan@gmail.com>
2020-04-28 10:31:44 +0700

    commit 1

---
+++ tugasmodul5_kel28/tugasmodul5_kel28/tugasmodul5_kel28.py
@@ -0,0 +1,7 @@
+from userService import userService #untuk menyertakan berkas userService
+
+print("System Login Kelompok 28!\n")
+email = input("Email: ")
+password = input("Password: ")
+get_class = userService(email,password) #pembuatan objek dari class userService
+get_class.login()
---
+++ tugasmodul5_kel28/tugasmodul5_kel28/userService.py
@@ -0,0 +1,68 @@
+class userService(object):
+    def __init__(self, email, password): #pembuatan constructor
+
+        self.email = email
+        self.password = password
+        self.data = {
+
+            "farhankelompok28@gmail.com" : {
+                "email"     : "farhankelompok28@gmail.com",
+                "password"  : "12345",
+                "role"      : "superadmin"
+            },
+
+            "fiqikelompok28@gmail.com" : {
+                "email"     : "fiqikelompok28@gmail.com",
+                "password"  : "12345",
+                "role"      : "user"
+            }
+        }
+
+        self.history = {
+            "farhankelompok28@gmail.com" : {
+                "peminjaman_buku" : {'Fisika Dasar','Dasar Komputer dan Pemrograman'},
+                "tanggal_pinjam"  : "28-04-2020"
+            },
+
+            "fiqikelompok28@gmail.com" :{
+                "peminjaman_buku" : {'Kalkulus','Dasar Komputer dan Pemrograman','Konsep Jaringan Komputer'},
+                "tanggal_pinjam"  : "28-04-2020",
+                }
+
+            }     
+
+    def login(self): #pembuatan method login
+
+        get_data = self.checkCredentials() #pembuatan variabel baru untuk pengecekan data inputan user
+        get_history = self.checkCredentials2()
+        hist_buku = get_history['peminjaman_buku']
+        hist_date = get_history['tanggal_pinjam']
+
+        if get_data: #pengkondisian boolean untuk login user
+            print("\nWelcome ",get_data['role'])
+            print("Logged it as user email: ",get_data['email'], "\n")
+            print(get_data['email'], "meminjam buku :\n")
+
+            for buku in hist_buku:
+                print(buku)
+                print()
+            print("Tanggal Peminjaman : ", hist_date)
+
+        else:
+            print("\nInvalid Login!\n")
+
+    def checkCredentials(self): #method untuk mengecek data inputan user
+        for value in self.data:
+            if value == self.email:
+                get_data_user = self.data[value]
+                if self.password == get_data_user['password']:
+                     return get_data_user 
+                else:
+                     return False
+
+    def checkCredentials2(self): #method untuk mengecek data inputan user
+        for value in self.history:
+            if value == self.email:
+                return self.history[value]
+
+
